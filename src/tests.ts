a176d8a0f29c8d40b5dbec3e02f9132179bd54e5
aquapi <aquaplmc@gmail.com>
2024-01-11 07:05:12 +0700

    Longer testing pathname

--- src/tests.ts
+++ src/tests.ts
@@ -13,13 +13,13 @@ export default {
 
     "URL params": {
         expect: 3,
-        route: ['/user/:name', [0, 0, 0]],
-        path: '/user/reve'
+        route: ['/user/:name/and/:id', [0, 0, 0]],
+        path: '/user/reve/and/0'
     },
 
     "Wildcards": {
         expect: 4,
-        route: ['/wildcard/and/sth/else/*', [0, 0, 0, 0]],
-        path: '/wildcard/and/sth/else/90'
+        route: ['/info/:project/and/*', [0, 0, 0, 0]],
+        path: '/info/stric/and/wildcard'
     }
 } as Tests;
